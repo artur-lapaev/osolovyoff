df430fa441ebf39a8a1287137b794ab0ed89a9c8
artur-lapaev <artur-lapaev@bk.ru>
2018-10-28 18:16:50 +0300

    added fix for animation background

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,4 +1,5 @@
-(function() {
+(function () {
+    animateBackground()
     let elemMobile = document.querySelector(".content__mobile");
     let elemDesktop = document.querySelector(".content__desktop");
     let content = document.querySelector(".main__content");
@@ -7,13 +8,15 @@
         showButtonHome(true, "mobile");
 
         let animationMobile = elemMobile.animate([
-            { top: '0',
-              transform: 'scale(1)',
-              opacity: '1'
+            {
+                top: '0',
+                transform: 'scale(1)',
+                opacity: '1'
             },
-            { top: '-70px',
-              transform: 'scale(0.5)',
-              opacity: '0'
+            {
+                top: '-70px',
+                transform: 'scale(0.5)',
+                opacity: '0'
             }
         ], 700);
         animationMobile.addEventListener("finish", () => {
@@ -21,15 +24,17 @@
             elemMobile.style.display = "none";
 
         });
-        
+
         let animationDesktop = elemDesktop.animate([
-            { top: '0',
-              transform: 'scale(1)',
-              opacity: '1'
+            {
+                top: '0',
+                transform: 'scale(1)',
+                opacity: '1'
             },
-            { top: '-70px',
-              transform: 'scale(0.5)',
-              opacity: '0'
+            {
+                top: '-70px',
+                transform: 'scale(0.5)',
+                opacity: '0'
             }
         ], 700);
         animationDesktop.addEventListener("finish", () => {
@@ -46,9 +51,9 @@
             }
             //added childs element attr href and className for mobile icons
             content.childNodes[0].classList.add("content__app-store");
-            content.childNodes[0].childNodes[0].setAttribute("href","https://www.apple.com/lae/ios/app-store/");
+            content.childNodes[0].childNodes[0].setAttribute("href", "#");
             content.childNodes[1].classList.add("content__play-market");
-            content.childNodes[1].childNodes[0].setAttribute("href","https://play.google.com/store?hl=en");
+            content.childNodes[1].childNodes[0].setAttribute("href", "#");
         }, 800)
 
     }, false);
@@ -57,13 +62,15 @@
         showButtonHome(true, "mobile");
 
         let animationMobile = elemMobile.animate([
-            { top: '0',
-              transform: 'scale(1)',
-              opacity: '1'
+            {
+                top: '0',
+                transform: 'scale(1)',
+                opacity: '1'
             },
-            { top: '70px',
-              transform: 'scale(0.5)',
-              opacity: '0'
+            {
+                top: '70px',
+                transform: 'scale(0.5)',
+                opacity: '0'
             }
         ], 700);
         animationMobile.addEventListener("finish", () => {
@@ -71,15 +78,17 @@
             elemMobile.style.display = "none";
 
         });
-        
+
         let animationDesktop = elemDesktop.animate([
-            { top: '0',
-              transform: 'scale(1)',
-              opacity: '1'
+            {
+                top: '0',
+                transform: 'scale(1)',
+                opacity: '1'
             },
-            { top: '70px',
-              transform: 'scale(0.5)',
-              opacity: '0'
+            {
+                top: '70px',
+                transform: 'scale(0.5)',
+                opacity: '0'
             }
         ], 700);
         animationDesktop.addEventListener("finish", () => {
@@ -94,14 +103,14 @@
                 content.appendChild(createDIV);
             }
             //added childs element attr href and className for desktop icons
-            content.childNodes[0].classList.add("content__steam");  
-            content.childNodes[0].childNodes[0].setAttribute("href","./"); 
+            content.childNodes[0].classList.add("content__steam");
+            content.childNodes[0].childNodes[0].setAttribute("href", "#");
             content.childNodes[1].classList.add("content__windows");
-            content.childNodes[1].childNodes[0].setAttribute("href","./");
-            content.childNodes[2].classList.add("content__mac");   
-            content.childNodes[2].childNodes[0].setAttribute("href","./");   
+            content.childNodes[1].childNodes[0].setAttribute("href", "#");
+            content.childNodes[2].classList.add("content__mac");
+            content.childNodes[2].childNodes[0].setAttribute("href", "#");
             content.childNodes[3].classList.add("content__linux");
-            content.childNodes[3].childNodes[0].setAttribute("href","./");
+            content.childNodes[3].childNodes[0].setAttribute("href", "#");
         }, 800)
 
     }, false);
@@ -127,6 +136,56 @@
                 footerElem.style.opacity = "1";
             });
         } else return;
+    }
+
+    function animateBackground() {
+        let background = document.querySelector(".background");
+        let savePosition = {};
+
+        setInterval(function () {
+
+            if (!savePosition.hasOwnProperty("x")) {
+                let x = Math.floor(Math.random() * (80 - 0)) + 0;
+                let y = Math.floor(Math.random() * (80 - 0)) + 0;
+                let z = Math.floor(Math.random() * (80 - 0)) + 0;
+
+                let xa = x + x;
+                let ya = y + y;
+                let za = z + z;
+                
+                background.animate([{
+                    transform: `translate3d(${x}px,${y}px,${z}px)`
+                }, {
+                    transform: `translate3d(${xa}px,${ya}px,${za}px)`
+                }], 1500);
+
+                savePosition.x = xa;
+                savePosition.y = ya;
+                savePosition.z = za;
+
+                background.style.transform = `translate3d(${savePosition.x}px,${savePosition.y}px,${savePosition.z}px)`;
+
+            } else {
+                
+                let x = Math.floor(Math.random() * (80 - 0)) + 0;
+                let y = Math.floor(Math.random() * (80 - 0)) + 0;
+                let z = Math.floor(Math.random() * (80 - 0)) + 0;
+
+                background.animate([{
+                     transform: `translate3d(${savePosition.x}px,${savePosition.y}px,${savePosition.z}px)`
+                }, {
+                    transform: `translate3d(${x}px,${y}px,${z}px)`
+                }], 1500);
+
+                savePosition.x = x;
+                savePosition.y = y;
+                savePosition.z = z;
+
+                background.style.transform = `translate3d(${savePosition.x}px,${savePosition.y}px,${savePosition.z}px)`;
+
+            }
+
+        }, 1500);
     }
 
 }());
